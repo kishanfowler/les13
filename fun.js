e7c545335c2a4b6dc1be16d418008b025f6c881b
kishan <33085@ma-web.nl>
2021-11-08 12:51:12 +0100

    first commit

---
+++ fun.js
@@ -0,0 +1,7 @@
+function changeBG()
+{
+    document.body.style.background="orange"
+    document.getElementById("div").style.color="white"
+    document.getElementsByClassName('pompoen').style.height="500"
+    document.getElementsByClassName('pompoen').style.width="500"
+}
